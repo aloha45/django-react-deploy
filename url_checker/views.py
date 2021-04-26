da6f8a1551ad0b9cb43f80dbe69e904bb82b4d12
Cory <corys4139@gmail.com>
2021-04-26 14:04:13 -0400

    initial commit with django

---
+++ url_checker/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import urllib.request
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+
+@csrf_exempt
+def check_url(rquest):
+  try: 
+    url_status = urllib.request.urlopen(request.body.decode("utf-8")).getcode()
+  except:
+    return HttpResponse(": URL not working")
+  if (url_status== 200):
+    return HttpResponse("Yes! URL works!")
+  return HttpResponse(": URL not working")
